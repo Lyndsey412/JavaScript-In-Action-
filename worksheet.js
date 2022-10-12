8c6720af05e71e3e2ee5104e84101e9fad1bb7f2
Lyndsey412 <111444654+Lyndsey412@users.noreply.github.com>
2022-10-12 14:42:17 -0400

    All files created for initial repository + task one complete

---
+++ worksheet.js
@@ -0,0 +1,12 @@
+//Task One 
+var dayOfWeek = ('Monday');
+console.log(dayOfWeek)
+
+var dayOfWeek = ('Friday');
+console.log('I cant wait for', dayOfWeek) // cant figure out the punctuation 
+
+//Task Two 
+let animalInput = ('What is your favorite animal?');
+let colorInput = ('What is your favorite color?');
+console.log('Ive never seen a', colorInput + animalInput);
+
