0d2d2b85d030573417e578f4ca7d9af573179391
Lyndsey412 <111444654+Lyndsey412@users.noreply.github.com>
2022-10-13 10:24:28 -0400

    Variables Tasks completed + Starting on Conditionals

--- worksheet.js
+++ worksheet.js
@@ -1,3 +1,4 @@
+//Variables
 //Task One 
 var dayOfWeek = ('Monday');
 console.log(dayOfWeek)
@@ -6,7 +7,15 @@ var dayOfWeek = ('Friday');
 console.log('I cant wait for', dayOfWeek) // cant figure out the punctuation 
 
 //Task Two 
-let animalInput = ('What is your favorite animal?');
-let colorInput = ('What is your favorite color?');
-console.log('Ive never seen a', colorInput + animalInput);
+let animalInput = prompt('What is your favorite animal?');
+let colorInput = prompt('What is your favorite color?');
+console.log('Ive never seen a', colorInput,animalInput);
+
+//Conditionals 
+//Task One 
+//Breakfast Burrito
+//Chicken Salad
+//Steak Potatoes and Asparagus
+var timeOfDay = 1300
+var favoriteMeals
 
