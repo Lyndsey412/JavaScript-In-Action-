199da76cb3f2d72f6ff47ee248ae7a1fecfbb715
Lyndsey412 <111444654+Lyndsey412@users.noreply.github.com>
2022-10-13 13:44:06 -0400

    Coded out the two tasks for the conditionals portion with 99% success

--- worksheet.js
+++ worksheet.js
@@ -1,21 +1,53 @@
 //Variables
 //Task One 
-var dayOfWeek = ('Monday');
+let dayOfWeek = 'Monday';
 console.log(dayOfWeek)
 
-var dayOfWeek = ('Friday');
-console.log('I cant wait for', dayOfWeek) // cant figure out the punctuation 
+dayOfWeek = ('Friday');
+console.log(`I cant wait for ${dayOfWeek}!`)
 
 //Task Two 
 let animalInput = prompt('What is your favorite animal?');
 let colorInput = prompt('What is your favorite color?');
-console.log('Ive never seen a', colorInput,animalInput);
+console.log(`Ive never seen a ${colorInput} ${animalInput}!`);
 
 //Conditionals 
 //Task One 
 //Breakfast Burrito
 //Chicken Salad
 //Steak Potatoes and Asparagus
-var timeOfDay = 1300
-var favoriteMeals
+
+let timeOfDay = 2100
+let favoriteMeals;
+
+if (timeOfDay < 1200){
+    favoriteMeals ='Breakfast Burrito';
+}
+else if(timeOfDay >= 1200 || timeOfDay <= 1700){
+    favoriteMeals = 'Chicken Salad';
+}
+else if(timeOfDay > 1700){
+    favoriteMeals = 'Steak, Potatoes and Asparagus';
+}
+console.log(favoriteMeals)
+
+//Task Two
+let bestBands = Math.floor(Math.random() * 11)
+
+if (bestBands >= 0 && bestBands <=2){
+    bestBands = 'Beatles'
+}
+else if (bestBands >= 3 && bestBands <= 5){
+    bestBands = 'Stones'
+}
+else if (bestBands >= 6 && bestBands<= 8){
+    bestBands = 'Floyd'
+}
+else if (bestBands == 9 || 10){
+    bestBands = 'Hendrix'
+}
+console.log(bestBands)
+
+
+
 
